ce80c68a64f8f97316301121d002c63e14fa43f2
Dibble <DeflatedPickle@gmail.com>
2019-02-11 21:22:22 +0000

    Made a smaller flamingo

--- flamingo.pl
+++ flamingo.pl
@@ -2,61 +2,102 @@
 use strict;
 use warnings;
 
+# Occasionally a fancy flamingo
+# TODO: Add a monocle
+my $flamingo_big_top_hat = <<'END_TOP_HAT';
+                           _______
+                          |       |
+                          |       |
+                          |_______|
+                         <_________>
+END_TOP_HAT
+
+# In case the hat isn't shown, there needs to be something else in it's place
+my $flamingo_big_head = <<'END_HEAD';
+                           ,/""""\
+END_HEAD
+
+my $flamingo_big = <<'END_FLAMINGO';
+                          / #     \
+                         /_,/---\  \
+                        / //     \  \
+                        |+|,     |  |
+                         \\/     |  |
+                          `     /  /
+                               /  /
+                              /  |           _,""""""""\_
+                             |   \_        _/            \_
+                             |     '\    _/  /,  ,         \_
+                             |,      \__/     \;/\   ,       \
+                              `\,                 \,/\   ,/\,/\
+                                `\                    \,/      \
+                                  \_\      ,_/ ,_/              \
+                                    `\____/___/__/"""\___,      ,\
+                                         || |  \, \,     `\_, \/\;
+                                         |  |    \, \,      '\_;
+                                         || |      \, \,
+                                         | ||        \, \,
+                                         |  |         _\, \,
+                                         || |        <_  '>
+                                         | ;|       ,/' ,/'
+                                         <  >      ,/ ,/`
+                                         |  |    ,/ ,/
+                                         || |  ,/ ,/
+                                         |__|,/ ,/
+                                         | ,/  /'
+                                         |/  /'
+                                        ,/ ,/'
+                                      ,/ ,/ |
+                                    ,/  /|  |
+END_FLAMINGO
+
+my $flamingo_small_top_hat = <<'END_FLAMINGO_SMALL_HEAD';
+                        ___
+                       |   |
+                       |___|
+                      <_____>
+END_FLAMINGO_SMALL_HEAD
+
+my $flamingo_small_head = <<'END_FLAMINGO_SMALL_HEAD';
+                        ___
+END_FLAMINGO_SMALL_HEAD
+
+my $flamingo_small = <<'END_FLAMINGO_SMALL';
+                       /~  \
+                      //--| |
+                      ;   /,/
+                        ,/ /   _/"""\
+                        |  "\_/ /,   '\
+                         \,      \_, \ \
+                          `\____,--,\/ \;
+                              ||\\    \;
+                              || \\
+                              || /,>
+                              ||//
+                              |//
+                              //
+                             //|
+END_FLAMINGO_SMALL
+
+my $flamingo_small_legs = <<'END_FLAMINGO_SMALL_LEGS';
+                            //||
+                           \| ||
+                            | ||
+                              ||
+                             <--\
+END_FLAMINGO_SMALL_LEGS
+
+# Get the input text
 my $text = <STDIN>;
 chomp $text;
 
-my $top_hat_chance = rand(60);
+my $top_hat_chance = int(rand(42));
 
-my $top_hat = << 'END_TOP_HAT';
-           _______
-          |       |
-          |       |
-          |_______|
-         <_________>
-END_TOP_HAT
-
-my $flamingo_head = << 'END_FLAMINGO_HEAD';
-           ,/""""\
-END_FLAMINGO_HEAD
-
-my $flamingo = << 'END_FLAMINGO';
-          / #     \
-         /_,/---\  \
-        / //     \  \
-        |+|,     |  |
-         \\/     |  |
-          `     /  /
-               /  /
-              /  |           _,""""""""\_
-             |   \_        _/            \_
-             |     '\    _/  /,  ,         \_
-             |       \__/     \;/\   ,       \
-              `\                  \,/\   ,/\,/\
-                `\                    \,/      \
-                  \_\      ,_/ ,_/              \
-                    `\____/___/__/"""\___       ,\
-                         || |  \, \,     `\_  \/\;
-                         |  |    \, \,      '\_;
-                         || |      \, \,
-                         || |        \, \,
-                         |  |         _\, \,
-                         || |        <_  '>
-                         |; |       ,/' ,/'
-                         <  >      ,/ ,/`
-                         |  |    ,/ ,/
-                         || |  ,/ ,/
-                         |__|,/ ,/
-                         | ,/  /'
-                         |/  /'
-                        ,/ ,/'
-                      ,/ ,/ |
-                    ,/  /|  |
-END_FLAMINGO
-
+# Print the text
 if ($top_hat_chance == 0) {
-    print $top_hat;
+    print $flamingo_small_top_hat;
 }
 else {
-    print $flamingo_head;
+    print $flamingo_small_head;
 }
-print $flamingo;
+print $flamingo_small;
