a9a292c3c796b40dbb80fb589a0ce66b8c06dbf1
Dibble <DeflatedPickle@gmail.com>
2019-02-12 02:53:32 +0000

    Added a speech box

--- flamingo.pl
+++ flamingo.pl
@@ -2,14 +2,102 @@
 use strict;
 use warnings;
 
-my $indent = "                        ";
+my $indent = "                    ";
+my $indent_small = "    ";
 
 sub indent_string {
+    my $text = "";
+
     foreach my $char (split(/\n/, $_[0])) {
-        print $_[1], $char, "\n";
+        $text = $text . $_[1] . $char . "\n";
     }
+
+    return $text;
 }
 
+sub craft_speech_bubble {
+    # TODO: Handle new lines
+    # my @text = split(/\n/, $_[0]);
+    my $text = $_[0];
+
+    my $speech_bubble = "";
+
+    my $width = 16;
+    my $height = 2;
+
+    foreach my $y (0..$height + 4) {
+        # print $y . "\n";
+
+        my @split_text = split(//, $text);
+
+        foreach my $x (0..$width) {
+            # print $x . "\n";
+
+            # Top Left/Bottom Right
+            if ($x == 0 && $y == 0 || $x == $width && $y == $height) {
+                $speech_bubble = $speech_bubble . "/";
+            }
+
+            # Top Middle
+            if ($x > 0 && $y == 0) {
+                $speech_bubble = $speech_bubble . "-";
+            }
+
+            # Fill
+            if ($x > 0 && $y > 0 && $x <= $width && $y < $height) {
+                if ($x > 1 && $x - 2 < int(length $text)) {
+                    $speech_bubble = $speech_bubble . $split_text[$x - 2];
+                }
+                else {
+                    $speech_bubble = $speech_bubble . " ";
+                }
+            }
+
+            # Left Side/Right Side
+            if ($x == 0 && $y > 0 && $y < $height || $x == $width && $y > 0 && $y < $height) {
+                $speech_bubble = $speech_bubble . "|";
+            }
+
+            # Top Right/Bottom Left
+            if ($x == $width && $y == 0 || $x == 0 && $y == $height) {
+                $speech_bubble = $speech_bubble . "\\";
+            }
+
+            # Bottom Middle
+            if ($x < $width - 3 && $y == $height) {
+                $speech_bubble = $speech_bubble . "-";
+            }
+            elsif ($x > $width - 3 && $y == $height) {
+                $speech_bubble = $speech_bubble . " ";
+            }
+
+            # Speech Triangle
+            if ($x >= 0 && $x < $width - 1 && $y > $height && $y < $height + 3) {
+                $speech_bubble = $speech_bubble . " ";
+            }
+
+            if ($x > $width - 3 && $x < $width - 1 && $y > $height && $y < $height + 2) {
+                $speech_bubble = $speech_bubble . "\\";
+            }
+
+            if ($x > $width - 5 && $x < $width - 3 && $y > $height - 1 && $y < $height + 1) {
+                $speech_bubble = $speech_bubble . ".";
+            }
+
+            if ($x == $width && $y > $height && $y < $height + 2) {
+                $speech_bubble = $speech_bubble . "/";
+            }
+        }
+
+        $speech_bubble = $speech_bubble . "\n";
+    }
+
+    return $speech_bubble;
+}
+
+# After walking into the editors' office,
+# you see, upon the desk, a jar of eyes in front of you...
+# Slowly, they all begin to twist and turn to look at you.
 my @eyes = ('o', '#', '+', '-', '*', '.', '~');
 
 # Occasionally a fancy flamingo
@@ -98,23 +186,24 @@ my $flamingo_small_legs = <<'END_FLAMINGO_SMALL_LEGS';
 END_FLAMINGO_SMALL_LEGS
 
 # Get the input text
-my $text = $ARGV[0];
-if ($text) {
-    chomp $text;
-}
+my $text = join(' ', @ARGV);
 
 my $top_hat_chance = int(rand(42));
 
+# This could have been done with an interpolated string,
+# but I don't feel like doubling up on the backslashes
 $flamingo_small =~ s/([\$])/$eyes[int(rand(scalar @eyes))]/g;
+
+print indent_string(craft_speech_bubble( $text), $indent_small);
 
 # Print the text
 if ($top_hat_chance == 0) {
     # print $flamingo_small_top_hat;
-    indent_string($flamingo_small_top_hat, $indent)
+    print indent_string($flamingo_small_top_hat, $indent)
 }
 else {
     # print $flamingo_small_head;
-    indent_string($flamingo_small_head, $indent)
+    print indent_string($flamingo_small_head, $indent)
 }
 # print $flamingo_small;
-indent_string($flamingo_small, $indent);
+print indent_string($flamingo_small, $indent);
