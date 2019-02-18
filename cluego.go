ac19b117efa73caf01bf91400c2c6469782847c9
Dibble <DeflatedPickle@gmail.com>
2019-02-18 15:55:20 +0000

    Added movement actions

--- cluego.go
+++ cluego.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"time"
 )
 
 // A basic room with-in the house
@@ -42,7 +43,7 @@ func NewRoom(name, shortName string, width, height int) *Room {
 }
 
 // Position the room besides 4 others, in the order; north, east, south and west
-func (room Room) positionRoom(rooms ...*Room) {
+func (room *Room) positionRoom(rooms ...*Room) {
 	room.roomNorth = rooms[0]
 	room.roomEast = rooms[1]
 	room.roomSouth = rooms[2]
@@ -54,15 +55,28 @@ type AIController struct {
 	// The likeliness that this person will murder another
 	murderLikeliness int
 
+	// Complications
+	// These are character traits that will affect the actions people do, and their response to others
+	// They might also give them motives to murder someone, or be blamed for murder
+
 	// How sociable this person is -- if it's low, they're unlikely to talk
-	// to anyone, and the others might not even learn of their existence
+	// to anyone, and the others might not even learn of their existence, if it's high,
+	// most of the people will know of them by the time of the murder
 	sociability int
+	// How anxious this person is -- if it's low, they'll be more happy around
+	// others, and if it's high, they'll try to avoid crowds
+	anxiety int
+	// How much this person likes to hold onto objects -- if it's low, they won't mind
+	// dropping their objects, and if it's high, they'll try to hold onto it,
+	// even if it's the murder weapon
+	hoarding int
 }
 
 func NewAIController() *AIController {
 	return &AIController{
-		murderLikeliness: rand.Intn(1000),
+		murderLikeliness: rand.Intn(100),
 		sociability:      0,
+		anxiety:          0,
 	}
 }
 
@@ -85,6 +99,9 @@ type Person struct {
 
 	// The room the person is currently in
 	currentRoom *Room
+
+	// The actions this person has done over the night
+	actions []string
 }
 
 func NewPerson(name string, ai *AIController, room *Room) *Person {
@@ -95,6 +112,7 @@ func NewPerson(name string, ai *AIController, room *Room) *Person {
 		murderer:    false,
 		inventory:   []*Item{},
 		currentRoom: room,
+		actions:     []string{},
 	}
 }
 
@@ -105,9 +123,10 @@ type Item struct {
 
 func main() {
 	// Set the clock
-	clock := 12.00
+	minutes := 00
+	hours := 12
 
-	rand.Seed(42)
+	rand.Seed(time.Now().Unix())
 
 	// Define the rooms of the house
 	// TODO: Add hallways
@@ -139,20 +158,72 @@ func main() {
 		NewPerson("Colonel", NewAIController(), grandFoyer),
 	}
 
+	for _, peep := range people {
+		fmt.Printf("The %s enters the grand foyer, with a likelyness to murder of %d%%.\n", peep.name, peep.ai.murderLikeliness)
+	}
+
 	// Loop the game for 12 hours,
 	// the murder should happen after 8 hours
 	// and then some more actions might happen, to help confuse things
-	for clock < 24.00 {
-		fmt.Printf("Time: %f\n", clock)
+	for hours < 24 {
+		for minutes <= 60 {
+			fmt.Printf("\nThe clock strikes %02d:%02d PM.\n", hours, minutes)
 
-		// Generate a number for the actions people could do,
-		// check if they can do it,
-		// then roll a dice to see if they do it
-		for _, peep := range people {
-			fmt.Println(peep.name)
+			// Generate a number for the actions people could do,
+			// check if they can do it,
+			// then roll a dice to see if they do it
+			for _, peep := range people {
+				if !peep.alive {
+					continue
+				}
+
+				action := rand.Intn(5)
+				willDo := rand.Intn(6)
+
+				if willDo == 1 || willDo == 6 {
+					continue
+				}
+
+				// Non-Event-Based Actions:
+				// 0 - Move rooms north
+				// 1 - Move rooms east
+				// 2 - Move rooms south
+				// 3 - Move rooms west
+				// 4 - Pick up item
+				// 5 - Put down item
+
+				switch action {
+				case 0:
+					if peep.currentRoom.roomNorth != nil {
+						peep.currentRoom = peep.currentRoom.roomNorth
+						fmt.Printf("The %s enters the %s.\n", peep.name, peep.currentRoom.name)
+					}
+				case 1:
+					if peep.currentRoom.roomEast != nil {
+						peep.currentRoom = peep.currentRoom.roomEast
+						fmt.Printf("The %s enters the %s.\n", peep.name, peep.currentRoom.name)
+					}
+				case 2:
+					if peep.currentRoom.roomSouth != nil {
+						peep.currentRoom = peep.currentRoom.roomSouth
+						fmt.Printf("The %s enters the %s.\n", peep.name, peep.currentRoom.name)
+					}
+				case 3:
+					if peep.currentRoom.roomWest != nil {
+						peep.currentRoom = peep.currentRoom.roomWest
+						fmt.Printf("The %s enters the %s.\n", peep.name, peep.currentRoom.name)
+					}
+				case 4:
+				case 5:
+				}
+			}
+
+			// Increase the minutes
+			minutes += 5
 		}
 
-		// Increase the clock by a minute
-		clock += 00.10
+		// Increase the hours
+		minutes = 0
+		hours++
 	}
 }
